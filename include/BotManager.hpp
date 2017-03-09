ba90d775262f72ce64cc7bbd40f8d97970c7aa80
Dyrand <dyrand@inbox.com>
2017-03-09 13:54:20 -0500

    Indentation Fix

--- include/BotManager.hpp
+++ include/BotManager.hpp
@@ -12,44 +12,44 @@
 
 namespace dyr
 {
-    class BotManager
-    {
-        public:
-            BotManager(){}
-            BotManager(const std::string& default_config);
-            ~BotManager();
+ class BotManager
+ {
+     public:
+         BotManager(){}
+         BotManager(const std::string& default_config);
+         ~BotManager();
 
-            int getBotCount();
+         int getBotCount();
 
-            /*Returns the id of the bot created
-              -1 otherwise */
-            int createBot(std::string config_file = "");
-            bool deleteBot(int id);
-            bool exist(int id);
+         /*Returns the id of the bot created
+           -1 otherwise */
+         int createBot(std::string config_file = "");
+         bool deleteBot(int id);
+         bool exist(int id);
 
-            void connectBots(int delay);
-            void process_loop();
+         void connectBots(int delay);
+         void process_loop();
     
-            //Called by bot to indicate it's ready
-            void notify_ready(const int& id);
-            //Called by bot to indicate an error occured
-            void notify_error(const int& id, DyrError&& error); 
+         //Called by bot to indicate it's ready
+         void notify_ready(const int& id);
+         //Called by bot to indicate an error occured
+         void notify_error(const int& id, DyrError&& error); 
 
-        private:
-            std::string default_config_file;
+     private:
+         std::string default_config_file;
             
-            boost::mutex mtx;
+         boost::mutex mtx;
 
-            std::map<int, DyrBot> id_bot_map;
-            std::map<int, boost::thread> id_bot_thread;
+         std::map<int, DyrBot> id_bot_map;
+         std::map<int, boost::thread> id_bot_thread;
 
-            int generateID();
+         int generateID();
 
-            BotManager(const BotManager&) = delete; //Copy Constructor
-            BotManager(BotManager&& other) noexcept = delete; //Move Constructor
-            BotManager& operator=(const BotManager& other) = delete; //Copy Assignment
-            BotManager& operator=(BotManager&& other) noexcept = delete; //Move Assignment
-    };
+         BotManager(const BotManager&) = delete; //Copy Constructor
+         BotManager(BotManager&& other) noexcept = delete; //Move Constructor
+         BotManager& operator=(const BotManager& other) = delete; //Copy Assignment
+         BotManager& operator=(BotManager&& other) noexcept = delete; //Move Assignment
+ };
 }
 
 #endif /*BOT_MANAGER_HPP*/
